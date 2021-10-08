f12256d135ff1a0cd3e1a0e55a5f5caa4dd2180e
HianPraxedes <78980856+HianPraxedes@users.noreply.github.com>
2021-10-07 23:51:36 -0300

    Update Chinelo.java

--- trabalho/src/model/Chinelo.java
+++ trabalho/src/model/Chinelo.java
@@ -1,6 +1,20 @@
 package model;
 
+    /
+    * Dados da bota
+    * @author acer
+    * @Version 1.0(Out 2021)
+    */
+
 public class Chinelo extends Produto {
+
+    /
+     * Guarda os dados cadastrais do chinelo, sendo ela:
+     * (1)tipo de correia
+     * (2)estetica
+     * @param Chinelo
+     * @return tipo de correia, estetica
+     */
 
     String tipoCorreia;
     String estetica;
