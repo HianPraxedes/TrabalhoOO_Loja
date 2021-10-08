5586f435ad203bbf89433a5074b04ef7c9a6e8c4
HianPraxedes <78980856+HianPraxedes@users.noreply.github.com>
2021-10-08 00:28:51 -0300

    Update TelaPessoaController.java

--- trabalho/src/controller/TelaPessoaController.java
+++ trabalho/src/controller/TelaPessoaController.java
@@ -1,5 +1,11 @@
 package controller;
 
+/
+ * controle de Pessoa cadastrada
+ * @author acer
+ * @Version 1.0(Out 2021)
+ */
+
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
@@ -9,6 +15,14 @@ import view.TelaPessoa;
 import view.TelaCadastroPessoa;
 
 public class TelaPessoaController {
+    
+    /
+     * Verifica se o usuário está cadastrado
+     * se estiver: será redirecionado para outra tela
+     * senão: será redirecionado para tela de cadastro de pessoa
+     * @param verifica o cadastro
+     * @return tela de cadaatro de pessoa
+     */
     
     private final TelaPessoa view;
     
