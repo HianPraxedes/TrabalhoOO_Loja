667ee38ba3c83acd0e044f78fcbdf21dec98d726
HianPraxedes <78980856+HianPraxedes@users.noreply.github.com>
2021-10-08 00:00:13 -0300

    Update Tenis.java

--- trabalho/src/model/Tenis.java
+++ trabalho/src/model/Tenis.java
@@ -1,6 +1,21 @@
 package model;
 
+	/
+ 	* Dados do tenis
+ 	* @author acer
+ 	* @Version 1.0(Out 2021)
+ 	*/
+
 public class Tenis extends Produto {
+	
+	 /
+    	 * Guarda os dados cadastrais do tenis, sendo elas:
+    	 * (1)altura do cano
+    	 * (2)modelo
+    	 * @param tenis
+    	 * @return altura do cano, modelo
+    	 */ 
+
 	
 	Double alturaCano;
 	String modelo;
